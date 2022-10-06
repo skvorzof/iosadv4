39f64df459ceb201852bd8ea5c84678aadcbd361
Dima Skvortsov <4594651+skvorzof@users.noreply.github.com>
2022-10-06 18:14:18 +0300

    Initial Commit

---
+++ Iosadv4/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Iosadv4
+//
+//  Created by Dima Skvortsov on 06.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Iosadv4/Iosadv4App.swift
@@ -0,0 +1,17 @@
+//
+//  Iosadv4App.swift
+//  Iosadv4
+//
+//  Created by Dima Skvortsov on 06.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Iosadv4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
